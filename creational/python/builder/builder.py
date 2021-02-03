dc37d30e4bdf7cbf85803a3cac488fc7ce32b5a1
Angel Santiago Jaime Zavala <asjz2892@gmail.com>
2021-02-02 22:19:47 -0600

    feat(python): add first samples

---
+++ creational/python/builder/builder.py
@@ -0,0 +1,33 @@
+class Pizza:
+    def __init__(self, inches):
+        self.inches = inches
+        self.ingredientes = ["salsa de tomate", "queso"]
+
+    def __str__(self):
+        return f'Mi pizza es de {self.inches}" con los siguientes ingredientes: {", ".join(self.ingredientes[:-1])} y {self.ingredientes[-1]}'
+
+class PizzaBuilder():
+    
+    def __init__(self, inches):
+        self.pizza = Pizza(inches)
+
+    def addCheese(self):
+        self.pizza.ingredientes.append("doble queso")
+
+    def addPepperoni(self):
+        self.pizza.ingredientes.append("pepperoni")
+
+    def addSalami(self):
+        self.pizza.ingredientes.append("salami")
+
+    def addPimientos(self):
+        self.pizza.ingredientes.append("pimientos")
+
+    def addCebolla(self):
+        self.pizza.ingredientes.append("cebolla")
+
+    def addChampiñones(self):
+        self.pizza.ingredientes.append("champiñones")
+
+    def build(self):
+        return self.pizza
