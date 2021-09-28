c42b5225745ed214a63a7c7bfcf9335ee3de76b5
Angel Santiago Jaime Zavala <asjz2892@gmail.com>
2021-09-27 21:05:33 -0500

    feat(python): :sparkles: improve builder pattern examples

--- creational/python/builder/normal_builder/builder.py
+++ creational/python/builder/normal_builder/builder.py
@@ -1,5 +1,6 @@
 import abc
 
+# Product 1
 class Car:
     def __init__(self, seats=None, engine=None, trip_computer=None, gps=None) -> None:
         self.seats = seats
@@ -18,6 +19,26 @@ class Car:
 
     def set_gps(self, gps):
         self.gps = gps
+
+# Product 2        
+class Manual:
+    def __init__(self, desc_seats=None, desc_engine=None, desc_trip_computer=None, desc_gps=None) -> None:
+        self.desc_seats = desc_seats
+        self.desc_engine = desc_engine
+        self.desc_trip_computer = desc_trip_computer
+        self.desc_gps = desc_gps
+    
+    def set_seats(self, desc_seats):
+        self.desc_seats = desc_seats
+
+    def set_engine(self, desc_engine):
+        self.desc_engine = desc_engine
+
+    def set_trip_computer(self, desc_trip_computer):
+        self.desc_trip_computer = desc_trip_computer
+
+    def set_gps(self, desc_gps):
+        self.desc_gps = desc_gps
 
 
 # Builder interface
@@ -41,14 +62,18 @@ class Builder(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def set_gps(self, has_gps):
         pass
+    
+    @abc.abstractmethod
+    def get_result(self):
+        pass
 
-# ConcreteBuilder1
+# ConcreteBuilder 1
 class CarBuilder(Builder):
     def __init__(self) -> None:
         self.car = Car()
 
     def reset(self):
-        pass
+        self.car = Car()
 
     def set_seats(self, number):
         self.car.set_seats(number)
@@ -64,3 +89,26 @@ class CarBuilder(Builder):
     
     def get_result(self):
         return self.car
+
+# ConcreteBuilder 2
+class CarManualBuilder(Builder):
+    def __init__(self, ) -> None:
+        self.car_manual = Manual()
+
+    def reset(self):
+        self.car_manual = Manual()
+
+    def set_seats(self, desc_number):
+        self.car_manual.set_seats(desc_number)
+
+    def set_engine(self, desc_engine):
+        self.car_manual.set_engine(desc_engine)
+
+    def set_trip_computer(self, desc_km):
+        self.car_manual.set_trip_computer(desc_km)
+
+    def set_gps(self, desc_gps):
+        self.car_manual.set_gps(desc_gps)
+
+    def get_result(self):
+        return self.car_manual
